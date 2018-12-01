825e4c8163fa8021e990b6dc9fc2509447d8e5b4
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2018-12-01 22:38:59 +0100

    Import Bootstrap 4 as plugins individually

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,21 @@
 // dependencies of Bootstrap 4
-import 'jquery';
-import 'popper.js';
+import 'jquery'
+import 'popper.js'
 
-import 'bootstrap';
+// Bootstrap 4 (import plugins individually)
+import 'bootstrap/js/dist/util'
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/popover'
+
+// project code
+import './js/main';
 
 import './css/index.scss';
