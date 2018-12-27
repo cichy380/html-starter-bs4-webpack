0317489947f8b8199c1f44e918223b1eae199264
cichy380 <marcin.dobroszek@gmail.com>
2018-12-27 09:09:19 +0100

    Added configuration settings for the plugins

--- assets/scripts/plugins.js
+++ assets/scripts/plugins.js
@@ -1,12 +1,12 @@
 /**
- * @fileoverview Initialization of the jQuery plugins and managements of the configuration settings for them
+ * @fileoverview Initialization of the jQuery plugins
  */
+
+import { TOOLTIPS_SETTINGS } from './config'
 
 /**
  * Enable tooltips
  * Initialize all tooltips on a page by select them by their data-toggle attribute
  * https://getbootstrap.com/docs/4.1/components/tooltips/#example-enable-tooltips-everywhere
  */
-$('[data-toggle="tooltip"]').tooltip({
-  delay: {show: 500, hide: 100},
-})
+$('[data-toggle="tooltip"]').tooltip(TOOLTIPS_SETTINGS)
