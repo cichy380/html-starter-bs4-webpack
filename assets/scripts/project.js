b884e4f0428af7d79bc91a79cf5ebab930ce7764
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2019-01-29 19:22:04 +0100

    Changed demo header

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -2,3 +2,20 @@
  * @fileoverview Custom project code...
  */
 
+const $window = $(window)
+const $header = $('.header')
+
+
+// appearance of header shadow when scroll page
+
+let controlHeaderShadow = () => {
+  if ($window.scrollTop() > 0) {
+    $header.addClass('shadow')
+  }
+  else {
+    $header.removeClass('shadow')
+  }
+}
+
+controlHeaderShadow()
+$window.scroll(controlHeaderShadow)
