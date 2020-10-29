d8c67d508aa9a4037af85b62726cfa8c49870d98
Marcin Dobroszek <marcin.dobroszek@pgf.com.pl>
2020-10-29 23:47:00 +0100

    demo primitive

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,6 @@
 import './vendor/jquery'
 import './vendor/bootstrap'
 import './vendor/fontawesome'
+
+import primitive from 'primitive';
+primitive({input: '../images/placeholder-img-300x300.jpg', output: '#output'})
